935b2d4db055f8cd510b4dca6e73953704479d7f
Jeffrin Mathew Benny <jeffrinmathewbenny@gmail.com>
2021-10-01 19:15:32 +0530

    Adding Edit Functionality Form

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -26,11 +26,11 @@ namespace Vidly.Controllers
         public ActionResult New()
         {
             var membershiptypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel
+            var viewModel = new CustomerFormViewModel
             {
                 MembershipTypes = membershiptypes
             };
-            return View(viewModel);
+            return View("CustomerForm", viewModel);
         }
 
         [HttpPost]
@@ -56,6 +56,21 @@ namespace Vidly.Controllers
                 return HttpNotFound();
             }
             return View(customer);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if(customer==null)
+            {
+                return HttpNotFound();
+            }
+            var viewModel=new CustomerFormViewModel
+            {
+                Customer=customer,
+                MembershipTypes=_context.MembershipTypes.ToList()
+            };
+            return View("CustomerForm",viewModel);
         }
       
     }
