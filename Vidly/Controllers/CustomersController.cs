99413ff259b7dcb2461224ea684d5fd51f191965
Jeffrin Mathew Benny <jeffrinmathewbenny@gmail.com>
2021-10-12 09:41:09 +0530

    Added Validation Summary

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -28,6 +28,7 @@ namespace Vidly.Controllers
             var membershiptypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer=new Customer(),
                 MembershipTypes = membershiptypes
             };
             return View("CustomerForm", viewModel);
